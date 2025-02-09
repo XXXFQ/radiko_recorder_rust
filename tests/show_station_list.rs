569fe7bcac86cd3746312c06a9a6804ba7aaca50
ARM0930 <72128692+XXXFQ@users.noreply.github.com>
2025-02-09 21:38:22 +0900

    feat: パッケージ名を radiko_recorder から radiko_recorder_rust に変更

--- tests/show_station_list.rs
+++ tests/show_station_list.rs
@@ -4,7 +4,7 @@ use assert_cmd::Command;
 
 #[test]
 fn test_show_station_list() {
-    let mut cmd = Command::cargo_bin("radiko_recorder").expect("Binary exists");
+    let mut cmd = Command::cargo_bin("radiko_recorder_rust").expect("Binary exists");
     cmd.args(&["--station-list", "--area_id", "JP13"])
        .assert()
        .success();
